fc3824a000f8554c3372a3652b9b394ec59842ff
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 17:18:56 -0400

    Updates

---
+++ precache-manifest.f8e0934c63689944c289edc47e92719f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94490b209793573a79a3a3ff9f53190",
+    "url": "/Songs/index.html"
+  },
+  {
+    "revision": "6bf84021f090377b1e65",
+    "url": "/Songs/static/js/2.562a25c4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Songs/static/js/2.562a25c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366b9662427a87645232",
+    "url": "/Songs/static/js/main.95d58bb3.chunk.js"
+  },
+  {
+    "revision": "e9ba5ad01f8d61541d48",
+    "url": "/Songs/static/js/runtime-main.c64e2260.js"
+  }
+]);
